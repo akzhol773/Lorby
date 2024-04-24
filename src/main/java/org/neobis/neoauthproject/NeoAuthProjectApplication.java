ba78e01e30f6c955750344146459ad68f9c2b163
akzholbek <akzholbek0096@gmail.com>
2024-04-24 21:07:38 +0100

    added settings

---
+++ src/main/java/org/neobis/neoauthproject/NeoAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package org.neobis.neoauthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoAuthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoAuthProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/neobis/neoauthproject/NeoAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neobis.neoauthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoAuthProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
