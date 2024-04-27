547d358db8074a82e5bfafe11e3066e3cadf7920
akzholbek <akzholbek0096@gmail.com>
2024-04-27 18:19:57 +0100

    created user and role entities

---
+++ src/main/java/org/neobis/neoauthproject/entity/Role.java
@@ -0,0 +1,32 @@
+package org.neobis.neoauthproject.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Data
+@Table(name = "roles")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    List<User> users = new ArrayList<>();
+
+}
--- src/main/java/org/neobis/neoauthproject/entity/User.java
+++ src/main/java/org/neobis/neoauthproject/entity/User.java
@@ -1,4 +1,86 @@
 package org.neobis.neoauthproject.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+
+    @Column(nullable = false, unique = true)
+    String username;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    boolean isEnabled;
+
+    @CreationTimestamp
+    Date createdDate;
+
+    @UpdateTimestamp
+    Date updatedDate;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user's roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    List<Role> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for(Role role: roles){
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
 }
